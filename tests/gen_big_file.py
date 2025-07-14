87606ac4a86fb19065212b65e7f5b19ef204fd32
Vinz2008 <vincentbidarddelanoe@gmail.com>
2025-07-14 21:28:22 +0200

    add big file test

---
+++ tests/gen_big_file.py
@@ -0,0 +1,17 @@
+
+
+
+out_file = "big_file.rml"
+
+f = open(out_file, mode = "w")
+
+
+for i in range(0, 10000):
+    f.writelines(
+        ["let f{} a =".format(i), 
+         "\tif a == {} then".format(i),
+         "\t\ta * 6 + 3" 
+         "\telse f{} a - 1 ;;\n\n".format(i)]
+    )
+
+f.close()
