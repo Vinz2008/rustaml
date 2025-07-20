f5f8fa10d276e1fa293dd08cc79297a3c8b35396
Vinz2008 <vincentbidarddelanoe@gmail.com>
2025-07-20 17:03:04 +0200

    work on refactoring error printing

--- src/print_error.rs
+++ src/print_error.rs
@@ -9,14 +9,19 @@ use enum_tags::TaggedEnum;
 
 const PARSER_ERROR_OFFSET : u32 = 100;
 
+struct ErrorBasicInfos<'a> {
+    error_nb : u32,
+    range : Range<usize>,
+    filename : &'a str,
+    content : &'a str
+}
 
-
-fn print_number_parsing_failure(error_nb : u8, range : Range<usize>, filename : &str, content : &str, buf  : Vec<char>){
-    Report::build(ReportKind::Error, (filename, range))
-    .with_code(error_nb)
+fn print_number_parsing_failure(error_basic_infos : ErrorBasicInfos, buf  : Vec<char>){
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Failure when parsing number \"{:?}\"", buf))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
 fn nearest_op(s : &str) -> &'static str {
@@ -32,41 +37,48 @@ fn nearest_op(s : &str) -> &'static str {
     nearest
 }
 
-fn print_invalid_op_error(error_nb : u8, range : Range<usize>, filename : &str, content : &str, op : String){
+fn print_invalid_op_error(error_basic_infos : ErrorBasicInfos, op : String){
     let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Invalid operator \"{}\"", op))
-    .with_label(Label::new((filename, range.clone())).with_message("This operator doesn't exist").with_color(a))
+    .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("This operator doesn't exist").with_color(a))
     .with_note(format!("maybe you meant {}", nearest_op(&op)))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
-fn print_unexpected_char_error(error_nb : u8, range : Range<usize>, filename : &str, content : &str, c : char){
+fn print_unexpected_char_error(error_basic_infos : ErrorBasicInfos, c : char){
     let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Unexpected char \'{}\'", c))
-    .with_label(Label::new((filename, range.clone())).with_message("This character shouldn't be there").with_color(a))
+    .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("This character shouldn't be there").with_color(a))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
 pub fn print_lexer_error(lexer_error : LexerErr, filename : &Path, content : &str) -> ExitCode {
     // println!("Parsing error : {:?}", parser_error);
 
-    let error_nb = lexer_error.lexer_err_data.tag() as u8;
+    let error_nb = lexer_error.lexer_err_data.tag() as u32;
     let range = lexer_error.range;
     let filename_str = filename.to_str().unwrap();
 
+    let error_basic_infos = ErrorBasicInfos {
+        error_nb,
+        range,
+        filename: filename_str,
+        content: content
+    };
+
     match *lexer_error.lexer_err_data {
-        LexerErrData::NumberParsingFailure(b) => print_number_parsing_failure(error_nb, range, filename_str, content, *b),
-        LexerErrData::InvalidOp(s) => print_invalid_op_error(error_nb, range, filename_str, content, *s),
-        LexerErrData::UnexpectedEOF => print_unexpected_eof_error(error_nb as u32, range, filename_str, content),
-        LexerErrData::UnexpectedChar(c) => print_unexpected_char_error(error_nb, range, filename_str, content, c),
+        LexerErrData::NumberParsingFailure(b) => print_number_parsing_failure(error_basic_infos, *b),
+        LexerErrData::InvalidOp(s) => print_invalid_op_error(error_basic_infos, *s),
+        LexerErrData::UnexpectedEOF => print_unexpected_eof_error(error_basic_infos),
+        LexerErrData::UnexpectedChar(c) => print_unexpected_char_error(error_basic_infos, c),
     }
     
 
@@ -75,49 +87,53 @@ pub fn print_lexer_error(lexer_error : LexerErr, filename : &Path, content : &st
 
 // TODO : refactor this code ? (make only one function ?)
 
-fn print_unexpected_eof_error(error_nb : u32, range : Range<usize>, filename : &str, content : &str){
+fn print_unexpected_eof_error(error_basic_infos : ErrorBasicInfos){
     let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message("Unexpected end of file")
-     .with_label(Label::new((filename, range.clone())).with_message("There is here a EOF that should not be here").with_color(a))
+     .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("There is here a EOF that should not be here").with_color(a))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
-fn print_wrong_tok_error(error_nb : u32, range : Range<usize>, filename : &str, content : &str, expected_tok : TokenDataTag, got_tok : TokenData){
+fn print_wrong_tok_error(error_basic_infos : ErrorBasicInfos, expected_tok : TokenDataTag, got_tok : TokenData){
     let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Wrong Token : expected {:?} but got {:?}", expected_tok, got_tok))
-    .with_label(Label::new((filename, range.clone())).with_message("This is the wrong token").with_color(a))
+    .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("This is the wrong token").with_color(a))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
-fn print_unexpected_tok_error(error_nb : u32, range : Range<usize>, filename : &str, content : &str, tok : TokenData){
+fn print_unexpected_tok_error(error_basic_infos : ErrorBasicInfos, tok : TokenData){
     let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Unexpected Token {:?}", tok))
-    .with_label(Label::new((filename, range.clone())).with_message("This is the wrong token").with_color(a))
+    .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("This is the wrong token").with_color(a))
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
 }
 
-fn print_type_inference_error(error_nb : u32, range : Range<usize>, filename : &str, content : &str, arg_name : &str){
+fn print_type_inference_error(error_basic_infos : ErrorBasicInfos, arg_name : &str){
 let mut colors = ColorGenerator::new();
     let a = colors.next();
-    Report::build(ReportKind::Error, (filename, range.clone()))
-    .with_code(error_nb)
+    Report::build(ReportKind::Error, (error_basic_infos.filename, error_basic_infos.range.clone()))
+    .with_code(error_basic_infos.error_nb)
     .with_message(format!("Type inference error with the argument {:?}", arg_name))
-    .with_label(Label::new((filename, range.clone())).with_message("This argument's type couldn't be deduced from the body of the function").with_color(a))
+    .with_label(Label::new((error_basic_infos.filename, error_basic_infos.range.clone())).with_message("This argument's type couldn't be deduced from the body of the function").with_color(a))
     .with_note("Either add a type annotation, or change the body to disambiguate")
     .finish()
-    .print((filename, Source::from(content))).unwrap();
+    .print((error_basic_infos.filename, Source::from(error_basic_infos.content))).unwrap();
+}
+
+fn print_unknown_type_annotation(error_basic_infos : ErrorBasicInfos){
+    todo!()
 }
 
 pub fn print_parser_error(parser_error : ParserErr, filename : &Path, content : &str) -> ExitCode {
@@ -128,12 +144,19 @@ pub fn print_parser_error(parser_error : ParserErr, filename : &Path, content : 
     let range = parser_error.range;
     let filename_str = filename.to_str().unwrap();
 
+    let error_basic_infos = ErrorBasicInfos {
+        error_nb,
+        range,
+        filename: filename_str,
+        content: content
+    };
+
     match *parser_error.parser_err_data {
-        ParserErrData::UnexpectedEOF => print_unexpected_eof_error(error_nb, range, filename_str, content),
-        ParserErrData::WrongTok { expected_tok, got_tok } => print_wrong_tok_error(error_nb, range, filename_str, content, expected_tok, got_tok),
-        ParserErrData::UnexpectedTok {tok } => print_unexpected_tok_error(error_nb, range, filename_str, content, tok),
-        ParserErrData::TypeInferenceErr { arg_name } => print_type_inference_error(error_nb, range, filename_str, content, &arg_name),
-        ParserErrData::UnknownTypeAnnotation {  } => todo!(), // TODO
+        ParserErrData::UnexpectedEOF => print_unexpected_eof_error(error_basic_infos),
+        ParserErrData::WrongTok { expected_tok, got_tok } => print_wrong_tok_error(error_basic_infos, expected_tok, got_tok),
+        ParserErrData::UnexpectedTok {tok } => print_unexpected_tok_error(error_basic_infos, tok),
+        ParserErrData::TypeInferenceErr { arg_name } => print_type_inference_error(error_basic_infos, &arg_name),
+        ParserErrData::UnknownTypeAnnotation {  } => print_unknown_type_annotation(error_basic_infos), // TODO
         ParserErrData::NotFunctionTypeInAnnotationLet { name } => todo!(),
     };
 
