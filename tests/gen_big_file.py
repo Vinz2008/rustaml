98080db9234a019115a50cdd1191ff8903006c85
Vinz2008 <vincentbidarddelanoe@gmail.com>
2025-08-27 23:21:37 +0200

    add function to big file

--- tests/gen_big_file.py
+++ tests/gen_big_file.py
@@ -11,7 +11,12 @@ for i in range(0, 10000):
         ["let f{} a{} =".format(i, i), 
          "\tif a{} == {} then".format(i, i),
          "\t\ta{} * 6 + 3".format(i), 
-         "\telse f{} a{} - 1 ;;\n\n".format(i, i)]
+         "\telse f{} a{} - 1 ;;\n\n".format(i, i),
+         "let m{} b{} = match b{} with".format(i, i, i),
+         "\t| 3 -> \"test\"",
+         "\t| 0..1 -> \"other test\"",
+         "\t| n -> \"last\" ;; \n\n",
+         ]
     )
 
 f.close()
