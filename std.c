36f8559a86f0436f6fc25679fd75e45b13f6e288
Vinz2008 <vincentbidarddelanoe@gmail.com>
2026-01-19 15:24:03 +0100

    put the utf8 decoding to a separate function

--- std.c
+++ std.c
@@ -454,6 +454,89 @@ static void utf8_error(const char* msg, uint8_t* b){
 // the data are on the low 6 bits with the 2 highest bits as 0
 #define GET_CONTINUATION_BYTE_DATA(c) (c & 0x3F) // & 00111111
 
+static uint32_t utf8_decode_char(const char* s, size_t* pos, size_t bytes_len){
+    size_t current_pos = *pos;
+    uint8_t c = s[current_pos];
+    if ((c & 0x80) == 0){ // & 10000000
+        (*pos)++;
+        return (uint32_t)c;
+    }
+        
+    if (IS_CONTINUATION_BYTE(c)){
+        utf8_error("continuation byte not expected", &c);
+    }
+    uint32_t code_point;
+    if ((c & 0xE0) == 0xC0){ // & 11100000 == 11000000
+        // 2 bytes codepoints
+        if (current_pos + 1 >= bytes_len){
+            utf8_error("missing bytes", NULL);
+        }
+        uint8_t byte_1 = c;
+        uint8_t byte_2 = s[current_pos+1];
+        if (!IS_CONTINUATION_BYTE(byte_2)){
+            utf8_error("should be continuation byte", &byte_2);
+        }
+        // (byte_1 & 00011111) | (byte_2 & 01111111)
+        code_point = (((uint32_t)(byte_1 & 0x1F)) << 6) | (uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) ; 
+        if (code_point < 0x80) {
+            utf8_error("overlong encoding", NULL);
+        }
+        (*pos) += 2;
+    } else if ((c & 0xF0) == 0xE0){ // & 11110000 == 11100000
+        // 3 bytes codepoints
+        if (current_pos + 2 >= bytes_len){
+            utf8_error("missing bytes", NULL);
+        }
+        uint8_t byte_1 = c;
+        uint8_t byte_2 = s[current_pos+1];
+        uint8_t byte_3 = s[current_pos+2];
+        if (!IS_CONTINUATION_BYTE(byte_2)){
+            utf8_error("should be continuation byte", &byte_2);
+        }
+        if (!IS_CONTINUATION_BYTE(byte_3)){
+            utf8_error("should be continuation byte", &byte_3);
+        }
+        // (byte_1 & 00001111)
+        code_point = ((uint32_t)(byte_1 & 0xF) << 12) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) << 6) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_3));
+        if (code_point < 0x800) {
+            utf8_error("overlong encoding", NULL);
+        }
+                
+        (*pos) += 3;
+    } else if ((c & 0xF8) == 0xF0) { // & 11111000 == 11110000
+        // 4 bytes codepoints
+        if (current_pos + 3 >= bytes_len){
+            utf8_error("missing bytes", NULL);
+        }
+        uint8_t byte_1 = c;
+        uint8_t byte_2 = s[current_pos+1];
+        uint8_t byte_3 = s[current_pos+2];
+        uint8_t byte_4 = s[current_pos+3];
+        if (!IS_CONTINUATION_BYTE(byte_2)){
+            utf8_error("should be continuation byte", &byte_2);
+        }
+        if (!IS_CONTINUATION_BYTE(byte_3)){
+            utf8_error("should be continuation byte", &byte_3);
+        }
+        if (!IS_CONTINUATION_BYTE(byte_4)){
+            utf8_error("should be continuation byte", &byte_4);
+        }
+        // (byte_1 & 00000111)
+        code_point = ((uint32_t)(byte_1 & 0x7) << 18) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) << 12) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_3) << 6) | (uint32_t)GET_CONTINUATION_BYTE_DATA(byte_4);
+        if (code_point < 0x10000) {
+            utf8_error("overlong encoding", NULL);
+        }
+        (*pos) += 4;
+    } else {
+        utf8_error("unknown leading bits", &c);
+    }
+    if (code_point > 0x10FFFF || (code_point >= 0xD800 && code_point <= 0xDFFF)) {
+        utf8_error("invalid Unicode scalar", NULL);
+    }
+    
+    return code_point;
+}
+
 struct ListNode* __chars(const char* s){
     size_t bytes_len = strlen(s);
     if (bytes_len == 0){
@@ -469,87 +552,12 @@ struct ListNode* __chars(const char* s){
         i++;
     }
 
-    while (i < bytes_len){
-        uint8_t c = s[i];
+    // no need to use INTO_TYPE because it is just widening all unsigned (so just zext)
 
-        if ((c & 0x80) == 0){ // & 10000000
-            // no need to use INTO_TYPE because it is just widening all unsigned (so just zext)
-            list_builder_append_back(&list_builder, CHAR_TYPE, (Val)c);
-            i++;
-        } else {
-            if (IS_CONTINUATION_BYTE(c)){
-                utf8_error("continuation byte not expected", &c);
-            }
-            uint32_t code_point;
-            if ((c & 0xE0) == 0xC0){ // & 11100000 == 11000000
-                // 2 bytes codepoints
-                if (i + 1 >= bytes_len){
-                    utf8_error("missing bytes", NULL);
-                }
-                uint8_t byte_1 = c;
-                uint8_t byte_2 = s[i+1];
-                if (!IS_CONTINUATION_BYTE(byte_2)){
-                    utf8_error("should be continuation byte", &byte_2);
-                }
-                // (byte_1 & 00011111) | (byte_2 & 01111111)
-                code_point = (((uint32_t)(byte_1 & 0x1F)) << 6) | (uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) ; 
-                if (code_point < 0x80) {
-                    utf8_error("overlong encoding", NULL);
-                }
-                i += 2;
-            } else if ((c & 0xF0) == 0xE0){ // & 11110000 == 11100000
-                // 3 bytes codepoints
-                if (i + 2 >= bytes_len){
-                    utf8_error("missing bytes", NULL);
-                }
-                uint8_t byte_1 = c;
-                uint8_t byte_2 = s[i+1];
-                uint8_t byte_3 = s[i+2];
-                if (!IS_CONTINUATION_BYTE(byte_2)){
-                    utf8_error("should be continuation byte", &byte_2);
-                }
-                if (!IS_CONTINUATION_BYTE(byte_3)){
-                    utf8_error("should be continuation byte", &byte_3);
-                }
-                // (byte_1 & 00001111)
-                code_point = ((uint32_t)(byte_1 & 0xF) << 12) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) << 6) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_3));
-                if (code_point < 0x800) {
-                    utf8_error("overlong encoding", NULL);
-                }
-                
-                i += 3;
-            } else if ((c & 0xF8) == 0xF0) { // & 11111000 == 11110000
-                // 4 bytes codepoints
-                if (i + 3 >= bytes_len){
-                    utf8_error("missing bytes", NULL);
-                }
-                uint8_t byte_1 = c;
-                uint8_t byte_2 = s[i+1];
-                uint8_t byte_3 = s[i+2];
-                uint8_t byte_4 = s[i+3];
-                if (!IS_CONTINUATION_BYTE(byte_2)){
-                    utf8_error("should be continuation byte", &byte_2);
-                }
-                if (!IS_CONTINUATION_BYTE(byte_3)){
-                    utf8_error("should be continuation byte", &byte_3);
-                }
-                if (!IS_CONTINUATION_BYTE(byte_4)){
-                    utf8_error("should be continuation byte", &byte_4);
-                }
-                // (byte_1 & 00000111)
-                code_point = ((uint32_t)(byte_1 & 0x7) << 18) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_2) << 12) | ((uint32_t)GET_CONTINUATION_BYTE_DATA(byte_3) << 6) | (uint32_t)GET_CONTINUATION_BYTE_DATA(byte_4);
-                if (code_point < 0x10000) {
-                    utf8_error("overlong encoding", NULL);
-                }
-                i += 4;
-            } else {
-                utf8_error("unknown leading bits", &c);
-            }
-            if (code_point > 0x10FFFF || (code_point >= 0xD800 && code_point <= 0xDFFF)) {
-                utf8_error("invalid Unicode scalar", NULL);
-            }
-            list_builder_append_back(&list_builder, CHAR_TYPE, (Val)code_point);
-        }
+
+    while (i < bytes_len){
+        uint32_t code_point = utf8_decode_char(s, &i, bytes_len);
+        list_builder_append_back(&list_builder, CHAR_TYPE, (Val)code_point);
     }
 
     size_t used_nodes = list_builder.nodes_buf_idx;
